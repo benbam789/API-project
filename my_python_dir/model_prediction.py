3e31e3b871948a9d2f71cae083e3c5cf18b7dd58
benbam789 <benanu1zaku.gmail.com>
2021-04-23 04:24:23 -0400

    2 working endpoints

--- my_python_dir/model_prediction.py
+++ my_python_dir/model_prediction.py
@@ -4,14 +4,22 @@ import numpy as np
 import json
 from flask import send_file
 import os
+from flask import request
+
+UPLOAD_FOLDER='.'
 
 #load the model
 
 my_model = load('log_model.pkl')
 
-#def download(filename):
- #   path = os.path.join(UPLOAD_FOLDER, filename)
-  #  return send_file(path, as_attachment=True)
+def upload(filename):
+    f = request.files['file']
+    f.save(filename)
+    return "Upload success"
+
+def download(filename):
+    path = os.path.join(UPLOAD_FOLDER, filename)
+    return send_file(path, as_attachment=True)
 
 def heartfailure(arg1):
     dummy = np.array(arg1)
